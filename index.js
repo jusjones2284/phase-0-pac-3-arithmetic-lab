4aa07b9e766dc691fa33bfeb92468732980186c4
Justin Jones <jus.jones@yahoo.com>
2022-02-12 19:44:07 -0500

    commit tree 65b0b60f0859929bb7bb74774cb2ab8606372730

---
+++ index.js
@@ -0,0 +1,78 @@
+/*number = 10;
+
+function add5() {
+    return(number += 5);
+}
+
+function divideBy3(){
+    return(number /= 3);
+}
+
+divideBy3();
+add5();
+
+console.log(number)
+
+
+number = 10;
+
+console.log(number)
+
+
+add5();
+divideBy3();
+
+console.log(number)
+
+number ++;
+number += 10;
+console.log(number)
+*/
+
+
+number = 50;
+
+function add() {
+    return(number += 10);
+}
+
+function subtract(){
+    return(number -= 5);
+}
+
+function multiply(){
+    return(number *= 10)
+}
+
+function divide(){
+    return(number /= 50);
+}
+
+function add(a, b){
+    return(a + b);
+}
+
+function subtract(a,b){
+    return(a - b);
+}
+
+function multiply(a,b){
+    return(a * b);
+}
+
+function divide(a,b){
+    return(a / b);
+}
+
+function  increment(n){
+    return(++n);
+}
+
+function decrement(n){
+    return(--n);
+}
+
+function makeInt(n){
+    const parsed = makeInt(n);
+    return parsed
+}
